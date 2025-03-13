fe4f7d3cd464f77ee67788dfb833cb498c64f0b8
Felipe J. <35155805+felipejo27@users.noreply.github.com>
2025-03-13 01:07:36 -0300

    Inativando por enquanto o acesso aos Data Source

--- src/main/java/com/example/demo/DesbravaConnectApplication.java
+++ src/main/java/com/example/demo/DesbravaConnectApplication.java
@@ -2,13 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//INATIVANDO O DATA SOURCE POR ENQUANTO
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class DesbravaConnectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesbravaConnectApplication.class, args);
+
 	}
 
-	
 }
